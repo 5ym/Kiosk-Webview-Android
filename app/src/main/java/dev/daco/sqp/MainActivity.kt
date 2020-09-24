cf50b459c06da43a7a9a11c4a5b2babfebcba66f
Yui Maruyama <16yuim@gmail.com>
2020-09-24 17:31:13 +0900

    いくつかの機能実装,画面固定動作修正

--- app/src/main/java/dev/daco/sqp/MainActivity.kt
+++ app/src/main/java/dev/daco/sqp/MainActivity.kt
@@ -2,28 +2,27 @@ package dev.daco.sqp
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
+import android.app.KeyguardManager
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager
-import android.webkit.*
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var kioskUtils: KioskUtils
     private val REQUEST_PERMISSION = 1000
-    private val REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,38 +52,21 @@ class MainActivity : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         kioskUtils = KioskUtils(this)
 
-        kioskUtils.start(this)
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.webChromeClient = object : WebChromeClient() {
             override fun onPermissionRequest(request: PermissionRequest) {
                 request.grant(request.resources)
             }
         }
-        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        myWebView.webViewClient = object : WebViewClient() {
-            @TargetApi(Build.VERSION_CODES.N)
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                return handleOverrideUrlLoading(view, request?.url)
-            }
-            @SuppressWarnings("deprecation")
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                return handleOverrideUrlLoading(view, Uri.parse(url))
-            }
-            fun handleOverrideUrlLoading(view: WebView?, uri: Uri?): Boolean {
-                if(uri?.scheme.equals("intent")) {
-                    val intent = Intent.parseUri(uri.toString(), Intent.URI_INTENT_SCHEME)
-                    val packageManager = this@MainActivity.packageManager
-                    if (packageManager != null && intent?.resolveActivity(packageManager) != null) {
-                        Log.v("intent", intent.extras.toString())
-                        startActivityForResult(intent, REQUEST_CODE)
-                        return true
-                    }
-                }
-                return false
-            }
-        }
+        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
         myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("https://sqp.sub.daco.dev")
+        val uri = this.intent.data
+        if(uri != null) {
+            myWebView.loadUrl(uri.toString())
+        } else {
+            kioskUtils.start(this)
+            myWebView.loadUrl("https://sqp.sub.daco.dev")
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
