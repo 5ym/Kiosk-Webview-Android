e1e46715528d4d7d0e1e6cc552a5891807de3256
Yui Maruyama <16yuim@gmail.com>
2020-09-25 00:09:19 +0900

    few fix

--- app/src/main/java/dev/daco/sqp/MainActivity.kt
+++ app/src/main/java/dev/daco/sqp/MainActivity.kt
@@ -2,12 +2,12 @@ package dev.daco.sqp
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.KeyguardManager
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import android.webkit.PermissionRequest
@@ -21,7 +21,6 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var kioskUtils: KioskUtils
     private val REQUEST_PERMISSION = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,26 +30,37 @@ class MainActivity : AppCompatActivity() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             startapp()
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSION
+            )
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
         if (requestCode == REQUEST_PERMISSION) {
             // 使用が許可された
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startapp()
             } else {
                 val context: Context = applicationContext
-                Toast.makeText(context , "許可されないとアプリが実行できません", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "許可されないとアプリが実行できません", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun startapp() {
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        kioskUtils = KioskUtils(this)
+        hideSystemUI()
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_SECURE
+        )
 
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.webChromeClient = object : WebChromeClient() {
@@ -58,28 +68,14 @@ class MainActivity : AppCompatActivity() {
                 request.grant(request.resources)
             }
         }
-        myWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
         myWebView.settings.javaScriptEnabled = true
         val uri = this.intent.data
         if(uri != null) {
             myWebView.loadUrl(uri.toString())
         } else {
-            kioskUtils.start(this)
+            this@MainActivity.startLockTask()
             myWebView.loadUrl("https://sqp.sub.daco.dev")
         }
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
-        return true
-    }
-
-    override fun onKeyLongPress(keyCode: Int, event: KeyEvent): Boolean {
-        return true
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        hideSystemUI()
     }
 
     private fun hideSystemUI() {
@@ -92,5 +88,15 @@ class MainActivity : AppCompatActivity() {
                         or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 )
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        Log.v("bkey", event.toString())
+        return true
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        hideSystemUI()
     }
 }
